e0e70e5374c677f672a726fb745ed99b3d2e8126
varunr333111 <mogullapallishivani@gmail.com>
2021-03-27 22:29:23 -0400

    Init

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,33 +2,24 @@ import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
+import LoginScreen from '../screens/login';
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
-
-// If you are not familiar with React Navigation, we recommend going through the
-// "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
+    <NavigationContainer>
+      <MainStackNavigator />
     </NavigationContainer>
   );
 }
-
 // A root stack navigator is often used for displaying modals on top of all other content
 // Read more here: https://reactnavigation.org/docs/modal
-const Stack = createStackNavigator<RootStackParamList>();
-
-function RootNavigator() {
+const MainStack = createStackNavigator();
+function MainStackNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
+    <MainStack.Navigator screenOptions={{
+    headerShown: false}}>
+      <MainStack.Screen name="login"
+        component={LoginScreen}/>
+    </MainStack.Navigator>
   );
 }
---
+++ screens/login.tsx
@@ -0,0 +1,125 @@
+import * as React from 'react';
+import { StyleSheet,ScrollView, Keyboard, Text,TouchableWithoutFeedback, StatusBar,View, TextInput, TouchableOpacity ,Image} from 'react-native';
+
+const DismissKeyboard = ({ children }) => (
+<TouchableWithoutFeedback 
+accessible={false}
+onPress={() => Keyboard.dismiss() }> {children}
+</TouchableWithoutFeedback>
+);
+
+export default function login() {
+
+   return (
+     
+       <>
+       <StatusBar
+        animated={true}
+        backgroundColor="#0054aa"
+        hidden={false} />
+        <ScrollView contentContainerStyle={{backgroundColor:'#ffffff',flex:1}} 
+        keyboardShouldPersistTaps='handled'>
+      
+       <View style={styles.header}>
+       <Image
+          style={styles.tinyLogo}
+          source={require('../Images/logo.png')}
+        />
+        </View>
+      <View style={styles.container}>
+        <View style={styles.textLabel} >
+            <Text style={styles.textStyle}>Email/userId</Text>
+        </View>
+        <View style={styles.inputView} >
+         
+        <TextInput  
+            style={styles.inputText}
+            placeholder="UserID" 
+            placeholderTextColor="#003f5c"
+        />
+        </View>
+        <View style={styles.textLabel} >
+            <Text style={styles.textStyle}>Password</Text>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password" 
+            placeholderTextColor="#003f5c"
+            />
+        </View>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
+      
+      </>
+    );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop:StatusBar.currentHeight,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    marginTop:150,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tinyLogo: {
+    resizeMode:"contain",
+    width:200
+  },
+  textStyle:{
+    fontWeight:'bold',
+    fontSize:16
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  textLabel:{
+    width:"80%",
+    height:20,
+    marginBottom:15,
+    justifyContent:"center",
+  },
+  inputView:{
+    width:"80%",
+    borderColor:"#979797",
+    borderWidth:1.5,
+    borderRadius:6,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    fontWeight:'bold',
+    height:50,
+    color:"#170500"
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#0054aa",
+    borderRadius:6,
+    height:50,
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white",
+    fontWeight:'bold',
+    fontSize:18,
+    textAlign: "center",
+  }
+});
