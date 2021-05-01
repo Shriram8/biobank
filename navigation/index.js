3009721d3ce1e41dbef0b915b2d30abaa3276b6d
Nikhil2896 <71423508+Nikhil2896@users.noreply.github.com>
2021-05-01 10:04:24 +0530

    backgrounduser screen added

--- navigation/index.js
+++ navigation/index.js
@@ -23,7 +23,7 @@ import {
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
-import {    ColorSchemeName, Image, Text, View } from "react-native";
+import { ColorSchemeName, Image, Text, View } from "react-native";
 import LoginScreen from "../screens/login";
 import HomeScreen from "../screens/homeScreen";
 import ProcessScreen from "../screens/processScreen";
@@ -38,30 +38,36 @@ import { Button } from "react-native-paper";
 import Alerts from "../screens/Alerts";
 import HistoryScreen from "../screens/HistoryScreen";
 import Logout from "../screens/Logout";
- 
- 
+import Users from "../screens/Users";
+
 const Drawer = createDrawerNavigator();
-const Navigation = (props) => { 
+const Navigation = (props) => {
   return (
     <NavigationContainer>
       {/* <MainStackNavigator isLoggedIn={props.isLoggedIn}  /> */}
       {props.isLoggedIn ? (
         <Drawer.Navigator
-        drawerContentOptions={{
-          activeBackgroundColor:"#f1f1f1",
-          activeTintColor: '#010101',
-          // labelStyle:{ alignSelf:'center'},
-          itemStyle: {  },
-        }}
-        initialRouteName="Home">
-          <Drawer.Screen 
-          headerShown={true}
-          options={{
-            headerShown:true,
-            drawerIcon:(()=><MaterialCommunityIcons  name={"home"}  size={30} color="#010101"/>),
-            headerRight: () => (
+          drawerContentOptions={{
+            activeBackgroundColor: "#f1f1f1",
+            activeTintColor: "#010101",
+            // labelStyle:{ alignSelf:'center'},
+            itemStyle: {},
+          }}
+          initialRouteName="Home"
+        >
+          <Drawer.Screen
+            headerShown={true}
+            options={{
+              headerShown: true,
+              drawerIcon: () => (
+                <MaterialCommunityIcons
+                  name={"home"}
+                  size={30}
+                  color="#010101"
+                />
+              ),
+              headerRight: () => (
                 <Button
-                  
                   icon={() => (
                     <MaterialCommunityIcons
                       name="bell"
@@ -70,31 +76,69 @@ const Navigation = (props) => {
                     />
                   )}
                 ></Button>
-              )
-          }}
-          name="Home" component={MainStackNavigator} />
-          <Drawer.Screen 
-          headerShown={true}
-          options={{
-            headerShown:true,
-            drawerIcon:(()=><MaterialCommunityIcons  name={"alert-box"}  size={30} color="#010101"/>)
-          }}
-          name="Alerts" component={Alerts} />
-          <Drawer.Screen 
-          headerShown={true}
-          options={{
-            headerShown:true,
-            drawerIcon:(()=><Image style={{height:30,width:30 }} source={require("./../assets/screenIcons/history.svg")} />) 
-          }}
-          name="History" component={HistoryScreen} />
-          <Drawer.Screen 
-          headerShown={true}
-          options={{
-            headerShown:true,
-            drawerIcon:(()=><Image style={{height:30,width:30 }} source={require("./../assets/screenIcons/logout.svg")} />) 
-          }}
-          name="Logout" component={Logout} />
-          
+              ),
+            }}
+            name="Home"
+            component={MainStackNavigator}
+          />
+          <Drawer.Screen
+            headerShown={true}
+            options={{
+              headerShown: true,
+              drawerIcon: () => (
+                <MaterialCommunityIcons
+                  name={"alert-box"}
+                  size={30}
+                  color="#010101"
+                />
+              ),
+            }}
+            name="Alerts"
+            component={Alerts}
+          />
+          <Drawer.Screen
+            headerShown={true}
+            options={{
+              headerShown: true,
+              drawerIcon: () => (
+                <Image
+                  style={{ height: 30, width: 30 }}
+                  source={require("./../assets/screenIcons/history.svg")}
+                />
+              ),
+            }}
+            name="History"
+            component={HistoryScreen}
+          />
+          <Drawer.Screen
+            headerShown={true}
+            options={{
+              headerShown: true,
+              drawerIcon: () => (
+                <MaterialCommunityIcons
+                  name={"account-box"}
+                  size={30}
+                  color="#010101"
+                />
+              ),
+            }}
+            name="Users"
+            component={Users}
+          />
+          <Drawer.Screen
+            headerShown={true}
+            options={{
+              headerShown: true,
+              drawerIcon: () => (
+                <Image
+                  style={{ height: 30, width: 30 }}
+                  source={require("./../assets/screenIcons/logout.svg")}
+                />
+              ),
+            }}
+            name="Logout"
+            component={Logout}
+          />
         </Drawer.Navigator>
       ) : (
         <MainStack.Navigator initialRouteName="login">
@@ -118,82 +162,73 @@ const MainStack = createStackNavigator();
 const MainStackNavigator = (props) => {
   return (
     <MainStack.Navigator initialRouteName="login">
-      
-        <>
-          <MainStack.Screen
-            name="homeScreen"
-            component={HomeScreen}
-            options={{
-              title: "",
-              headerStyle: {
-                elevation: 0,
-                backgroundColor: "#006bcc",
-              },
-              headerLeft: () => (
-                <Button
-                  onPress={() => {
-                     props.navigation.openDrawer();
-                  }}
-                  icon={() => (
-                    <MaterialCommunityIcons
-                      name="menu"
-                      size={30}
-                      color="white"
-                    />
-                  )}
-                ></Button>
-              ),
-              headerRight: () => (
-                <Button
-                  icon={() => (
-                    <MaterialCommunityIcons
-                      name="bell"
-                      size={30}
-                      color="white"
-                    />
-                  )}
-                ></Button>
-              ),
-            }}
-          />
-          <MainStack.Screen
-            name="preProcessScreen"
-            component={PreProcessScreen}
-            initialParams={{ operationTheaterName: "" }}
-            options={({ route }) => ({
-              title: route.params.operationTheaterName,
-              headerTitleAlign: "center",
-              headerStyle: {
-                backgroundColor: "white",
-              },
-            })}
-          />
-          <MainStack.Screen
-            name="processScreen"
-            component={ProcessScreen}
-            initialParams={{ resourceName: "" }}
-            options={({ route }) => ({
-              title: route.params.resourceName,
-              headerTitleAlign: "center",
-              headerStyle: {
-                backgroundColor: "white",
-              },
-            })}
-          />
-          <MainStack.Screen
-            name="questionsScreen"
-            component={QuestionsScreen}
-            initialParams={{ resourceName: "" }}
-            options={({ route }) => ({
-              title: route.params.resourceName,
-              headerTitleAlign: "center",
-              headerStyle: {
-                elevation: 0,
-                backgroundColor: "#ff8d48",
-              },
-            })}
-          />
-        </> 
+      <>
+        <MainStack.Screen
+          name="homeScreen"
+          component={HomeScreen}
+          options={{
+            title: "",
+            headerStyle: {
+              elevation: 0,
+              backgroundColor: "#006bcc",
+            },
+            headerLeft: () => (
+              <Button
+                onPress={() => {
+                  props.navigation.openDrawer();
+                }}
+                icon={() => (
+                  <MaterialCommunityIcons name="menu" size={30} color="white" />
+                )}
+              ></Button>
+            ),
+            headerRight: () => (
+              <Button
+                icon={() => (
+                  <MaterialCommunityIcons name="bell" size={30} color="white" />
+                )}
+              ></Button>
+            ),
+          }}
+        />
+        <MainStack.Screen
+          name="preProcessScreen"
+          component={PreProcessScreen}
+          initialParams={{ operationTheaterName: "" }}
+          options={({ route }) => ({
+            title: route.params.operationTheaterName,
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "white",
+            },
+          })}
+        />
+        <MainStack.Screen
+          name="processScreen"
+          component={ProcessScreen}
+          initialParams={{ resourceName: "" }}
+          options={({ route }) => ({
+            title: route.params.resourceName,
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "white",
+            },
+          })}
+        />
+        <MainStack.Screen
+          name="questionsScreen"
+          component={QuestionsScreen}
+          initialParams={{ resourceName: "" }}
+          options={({ route }) => ({
+            title: route.params.resourceName,
+            headerTitleAlign: "center",
+            headerStyle: {
+              elevation: 0,
+              backgroundColor: "#ff8d48",
+            },
+          })}
+        />
+      </>
     </MainStack.Navigator>
   );
 };
--- src/graphql/queries.tsx
+++ src/graphql/queries.tsx
@@ -1,162 +1,221 @@
 import { gql } from "@apollo/client";
 export const GetUserDetails = gql`
-  query($userID:String){
-      appUsers(where: { uid: $userID }){
-        id,
-        name,
-        userType,
-        password,
-      }
+  query($userID: String) {
+    appUsers(where: { uid: $userID }) {
+      id
+      name
+      userType
+      password
     }
+  }
 `;
 
 export const GetResourcesDetails = gql`
-query{
-      appResources{
-        id,
-        name,
-      }
+  query {
+    appResources {
+      id
+      name
     }
+  }
+`;
+
+export const GetStaffUsers = gql`
+  query {
+    appUsers(where: { userType: "OTStaff" }) {
+      id
+      name
+      userType
+    }
+  }
+`;
+
+export const GetAdminUsers = gql`
+  query {
+    appUsers(where: { userType: "OTAdmin" }) {
+      id
+      name
+      userType
+    }
+  }
+`;
+
+export const GetInchargeUsers = gql`
+  query {
+    appUsers(where: { userType: "OTIncharge" }) {
+      id
+      name
+      userType
+    }
+  }
 `;
 
 export const GetProcessesDetails = gql`
-query($resourceID:ID!){
-      appResource(id: $resourceID){
-        process_details{
-          id,
-          Number,
-          process_name,
-          questions{
-            id
-          }
+  query($resourceID: ID!) {
+    appResource(id: $resourceID) {
+      process_details {
+        id
+        Number
+        process_name
+        questions {
+          id
         }
       }
     }
+  }
 `;
 
-
-
 export const GetQuestionDetails = gql`
-query($processID:ID!,
-      $operation_theater: ID!,
-      $app_user: ID!,
-      $instance: Int,
-      $Date:String){
-      processDetail(id: $processID){
-        id,
-    		questions{
-          id,
-          Question,
-          type,
-        }
-      }
-      processesData(where:{
-        app_user:$app_user,
-        process_detail:$processID,
-        operation_theater:$operation_theater,
-        instance:$instance,
-        Date:$Date}){
-        id,
-        check_editable{
-          id
-        },
-        question{
-          id
-        }
-        Answer,
-        created_at,
-        Date
+  query(
+    $processID: ID!
+    $operation_theater: ID!
+    $app_user: ID!
+    $instance: Int
+    $Date: String
+  ) {
+    processDetail(id: $processID) {
+      id
+      questions {
+        id
+        Question
+        type
       }
     }
+    processesData(
+      where: {
+        app_user: $app_user
+        process_detail: $processID
+        operation_theater: $operation_theater
+        instance: $instance
+        Date: $Date
+      }
+    ) {
+      id
+      check_editable {
+        id
+      }
+      question {
+        id
+      }
+      Answer
+      created_at
+      Date
+    }
+  }
 `;
 
 export const GetAnswersProgress = gql`
-query( $operation_theater: ID!
+  query(
+    $operation_theater: ID!
     $app_user: ID!
     $processID: ID!
     $instance: Int
-    $Date:Date){
-  processesData(where:{
-        app_user:$app_user,
-        process_detail:$processID,
-        operation_theater:$operation_theater,
-        instance:$instance,
-        Date:$Date}){
-        id,
-        Answer,
-        check_editable{
-          id
-        },
-        process_detail{
+    $Date: Date
+  ) {
+    processesData(
+      where: {
+        app_user: $app_user
+        process_detail: $processID
+        operation_theater: $operation_theater
+        instance: $instance
+        Date: $Date
+      }
+    ) {
+      id
+      Answer
+      check_editable {
         id
       }
+      process_detail {
+        id
       }
+    }
   }
 `;
 
-
 export const GetSharedResource_OperationTheaters = gql`
-query{
-      appResources(where:{resourceType:"SharedResource"}){
-        id,
-        name
-      },
-      operationTheaters{
-        id,
-      	name
-      }
+  query {
+    appResources(where: { resourceType: "SharedResource" }) {
+      id
+      name
     }
+    operationTheaters {
+      id
+      name
+    }
+  }
 `;
 
 export const preProcessProgress = gql`
-query($operation_theater:ID!,$app_user:ID!,$Date:Date,$instance:Int,$process_detail:ID!){
-    processesData(where:{operation_theater:$operation_theater,
-      app_user:$app_user,Date:$Date,instance:$instance,
-      process_detail:$process_detail,check_editable_null:false}){
-      id,
-      Answer,
-    	Date,
-    	process_detail{
+  query(
+    $operation_theater: ID!
+    $app_user: ID!
+    $Date: Date
+    $instance: Int
+    $process_detail: ID!
+  ) {
+    processesData(
+      where: {
+        operation_theater: $operation_theater
+        app_user: $app_user
+        Date: $Date
+        instance: $instance
+        process_detail: $process_detail
+        check_editable_null: false
+      }
+    ) {
+      id
+      Answer
+      Date
+      process_detail {
         id
       }
-    	check_editable{
+      check_editable {
         id
       }
-    	instance,
-    	operation_theater{
-        id,
+      instance
+      operation_theater {
+        id
         name
       }
     }
-}
+  }
 `;
 
 export const GetSurgeryDetails = gql`
-query($operation_theater:ID!,$app_user:ID!,$Date:Date){
-  appResources(sort: "processOrder:asc",where:{resourceType:"OperationTheater" }){
-        id,
-        name,
-  			processOrder,
-        process_details{
+  query($operation_theater: ID!, $app_user: ID!, $Date: Date) {
+    appResources(
+      sort: "processOrder:asc"
+      where: { resourceType: "OperationTheater" }
+    ) {
+      id
+      name
+      processOrder
+      process_details {
+        id
+      }
+    }
+    questions(where: { id: 6 }) {
+      processes_data(
+        where: {
+          operation_theater: $operation_theater
+          app_user: $app_user
+          Date: $Date
+        }
+      ) {
+        id
+        Answer
+        Date
+        operation_theater {
           id
         }
-  },
-	questions(where:{id:6}){
-    processes_data(where:{operation_theater:$operation_theater,app_user:$app_user,Date:$Date}){
-      id,
-      Answer,
-      Date,
-      operation_theater{
-        id
-      },
+      }
     }
   }
-}
 `;
 
 // export const GetPreProcessDetails = gql`
 // query($operationTheaterID:ID!){
-  
+
 // }
 // `;
 
@@ -166,66 +225,112 @@ export enum ENUM_RESOURCE_TYPE {
 }
 
 export const SubmitAnswerForQuestion = gql`
-mutation(
+  mutation(
     $operation_theater: ID!
     $question: ID!
     $app_user: ID!
     $process_detail: ID!
-    $Date:Date
-    $Answer:String
-    $instance:Int,
-  ){
-  createProcessesDatum(input: { data:{operation_theater:$operation_theater,
-    Answer:$Answer,
-    question:$question,app_user:$app_user,process_detail:$process_detail,Date:$Date,instance:$instance} }) {
-    processesDatum{
-      id,
-      Answer,
-      check_editable{
-        id
-      },
-      question{
-        id
-      },
-      operation_theater{
-        id
+    $Date: Date
+    $Answer: String
+    $instance: Int
+  ) {
+    createProcessesDatum(
+      input: {
+        data: {
+          operation_theater: $operation_theater
+          Answer: $Answer
+          question: $question
+          app_user: $app_user
+          process_detail: $process_detail
+          Date: $Date
+          instance: $instance
+        }
       }
-      created_at,
-      process_detail{
+    ) {
+      processesDatum {
         id
+        Answer
+        check_editable {
+          id
+        }
+        question {
+          id
+        }
+        operation_theater {
+          id
+        }
+        created_at
+        process_detail {
+          id
+        }
       }
     }
   }
-}
 `;
 
-export const SubmitCompleted =gql`
-mutation(
-  $processes_data:[ID]
-){
-  createCheckEditable(input:{data:{editable:true,processes_data:$processes_data}}){
-    checkEditable{
-      id,
-      editable
+export const SubmitCompleted = gql`
+  mutation($processes_data: [ID]) {
+    createCheckEditable(
+      input: { data: { editable: true, processes_data: $processes_data } }
+    ) {
+      checkEditable {
+        id
+        editable
+      }
     }
   }
-}
 `;
 
 export const UpdateSubmittedAnswerForQuestion = gql`
-mutation(
-    $question_Id: ID!
-    $Answer:String
-  ){
-  updateProcessesDatum(input: {
-    where: {id:$question_Id}
-    data:{Answer:$Answer} }){
-    processesDatum{
-      id,
-      Answer,
+  mutation($question_Id: ID!, $Answer: String) {
+    updateProcessesDatum(
+      input: { where: { id: $question_Id }, data: { Answer: $Answer } }
+    ) {
+      processesDatum {
+        id
+        Answer
+      }
     }
   }
-}
 `;
 
+export enum ENUM_APPUSERS_USERTYPE {
+  OTIncharge,
+  OTStaff,
+  OTAdmin,
+}
 
+export const addNewUser = gql`
+  mutation(
+    $name: String
+    $password: String!
+    $uid: String
+    $userType: ENUM_APPUSERS_USERTYPE
+  ) {
+    createAppUser(
+      input: {
+        data: {
+          name: $name
+          password: $password
+          uid: $uid
+          userType: $userType
+        }
+      }
+    ) {
+      appUser {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const deleteUser = gql`
+  mutation($id: ID!) {
+    deleteAppUser(input: { where: { id: $id } }) {
+      appUser {
+        id
+      }
+    }
+  }
+`;
