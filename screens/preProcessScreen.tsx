3333c6f40f0f9d504d55bd77eddb07027738373c
varunr333111 <varunr333@gmail.com>
2021-05-03 00:22:16 -0600

    PreprocessScreen Progress

--- screens/preProcessScreen.tsx
+++ screens/preProcessScreen.tsx
@@ -16,12 +16,19 @@ let lock: boolean[] = [];
 let moduleLock: boolean;
 var progress: any[] = [];
 var colorValue: any[] = [];
+var iconValue: any[] = [];
 var processCount: any[] = [];
 var netProgress: any[] = [];
 var _text: string;
 var red = "#f40000";
 var green = "#0fbb5b";
 var orange = "#ff8d48";
+var alert = "alert-box";
+var check = "checkbox-marked";
+var minusBox = "minus-box";
+var _Result: any [] = [];
+var _length: number;
+var _headerColor;
 
 export default function preProcessScreen({route, navigation}: {navigation: any, route:any}) {
     const { userId, operationTheaterID, operationTheaterName } = route.params;
@@ -30,19 +37,25 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
     const [refresh,setRefresh] = useState(0);
     const [updateMessage,setUpdateMessage] = useState(0);
     const [headerColor,setHeaderColor] = useState("")
+    const [headerIcon,setHeaderIcon]= useState("");
+    const [resultsFetched,setResultsFetched] = useState(0);
     const jewelStyle = (item: number | undefined)=>{
       return (item == 1)?{backgroundColor:"white"}:{backgroundColor:"#b6b6b6"}
     }
 
     React.useEffect(() => {
       const unsubscribe = navigation.addListener('focus', () => {
+        setMessage(null);
         processCount =[];
         progress = [];
         netProgress = [];
         _text = "Ongoing start of the day";
         _data = [];
         lock =[];
+        _Result = [];
+        _length = 0;
         colorValue = [];
+        iconValue = [];
         moduleLock = false;
         apolloClient
         .query({
@@ -55,7 +68,6 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
           fetchPolicy:"network-only"
         })
         .then((Result) => {
-          
             for(var i = 0;i<2;i++){
                 _data.push(Result.data.appResources[i]);
                 
@@ -68,7 +80,6 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
             }
             try{
               for(var i =0; i< Result.data.questions[0].processes_data[0].Answer;i++){
-                
                 _data.push(Result.data.appResources[preSurgeryProcessID-1]);
                 lock.push(true);
               }
@@ -80,9 +91,12 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
             //console.log("Data---",_data);
             setRenderFlatlistData(Result.data);
               for(var i= 0;i<_data.length;i++){
-                colorValue[i] = "#ff8d48";
+                colorValue[i] = orange;
+                iconValue[i] = minusBox;
                 processCount[i]=_data[i].process_details.length;
                 progress[i] = 0;
+                _length = _length + processCount[i];
+                console.log("III",i,"ProcessCount",processCount[i]);
                     for(var k=0;k<processCount[i];k++){
                       apolloClient
                       .query({
@@ -97,51 +111,11 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
                         fetchPolicy:"network-only"
                       })
                       .then((Result) => {
-                        //console.log("Result----data",Result);
-                        try{
-                          if(Result.data.processesData[0].check_editable){
-                            console.log("instance-----",Result.data.processesData[0]);
-                            var p = Result.data.processesData[0].instance;
-                            progress[p] = progress[p]+1;
-                            netProgress[p] = progress[p]/processCount[p];
-                            if(netProgress[p] == 1 ){
-                              if(p<preSurgeryProcessCount && !Result.data.processesData[0].check_editable.processCleared){
-                                moduleLock = true;
-                                console.log("Module Lock",moduleLock,"Process instance",p)
-                              }
-                              try{
-                                if(moduleLock == true && p == preSurgeryProcessCount-1){
-                                  console.log("MMMMMMM",)
-                                  lock[preSurgeryProcessCount] = true;
-                                  setRefresh(prevCount => prevCount + 1);
-                                }
-                                else lock[p+1] = false;
-                              }catch{
-
-                              }
-                            }else{
-                              //console.log(netProgress[p]);
-                            }
-                            if(netProgress[p]>0 && netProgress[p]<1){
-                              colorValue[p] = "#ff8d48" 
-                            }
-
-                            if(Result.data.processesData[0].check_editable.processCleared){
-                                colorValue[p] = "#0fbb5b" 
-                                setUpdateMessage(prevCount => prevCount + 1);
-                            }else {colorValue[p] = "#f40000"}
-                            
-                          }
-                          //console.log("Process---",processCount,"Progress---",progress)
-                          //console.log("net progress--",netProgress);
-                          setRefresh(prevCount => prevCount + 1);
-                          //console.log("----------Color--",colorValue);
-                          //console.log("Refresh",refresh);
+                        _Result.push(Result.data);
+                        if(_Result.length == _length){
+                          setResultsFetched(prevCount => prevCount + 1);
                         }
-                        catch{
-
-                        }
-                        
+                                                
                       })
                     }  
               }
@@ -151,26 +125,127 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
       return unsubscribe;
     }, [navigation]);
 
+    
     React.useEffect(()=>{
-      setMessage(_text)
-    },[updateMessage]);
+      console.log("GOT TATA")
+      if(_Result.length == _length){
+        console.log("GOT DATATA")
+        for(var k = 0;k<_Result.length;k++){
+                    try{
+                          if(_Result[k].processesData[0].check_editable){
+                            console.log("instance-----",_Result[k].processesData[0]);
+                            var p = _Result[k].processesData[0].instance;
+                            progress[p] = progress[p]+1;
+                            netProgress[p] = progress[p]/processCount[p];
+                            // if(netProgress[p] == 1 ){
+                              
+                            //   if(p<preSurgeryProcessCount && !_Result[k].processesData[0].check_editable.processCleared){
+                            //     moduleLock = true;
+                            //   }
+                            //   try{
+                            //     if(moduleLock == true && p == preSurgeryProcessCount-1){
+                            //       console.log("MMMMMMM",)
+                            //       lock[preSurgeryProcessCount] = true;
+                            //       //setRefresh(prevCount => prevCount + 1);
+                            //     }
+                            //     else lock[p+1] = false;
+                            //   }catch{
 
-    const changeIconSet=(id: number)=>{
-      try{
-        if(progress[id]){
-            if(netProgress[id] == 1){
-              return "checkbox-marked";
-            }
-            // else if(colorValue[id] == "#f40000")
-            //   return "alert-box"
-            else
-              return "minus-box"
+                            //   }
+                            // }else{
+                            //   //console.log(netProgress[p]);
+                            // }
+                            
+
+                            if(netProgress[p]==1 && _Result[k].processesData[0].check_editable.processCleared){
+                              if(colorValue[p] != red ){
+                                colorValue[p] = green 
+                                iconValue[p] = check;
+                                lock[p+1] = false;
+                                setUpdateMessage(prevCount => prevCount + 1);
+                              }   
+                            }
+                            if(netProgress[p]>0 && netProgress[p]<1){
+                              if(colorValue[p] != red ){
+                                colorValue[p] = orange;
+                                iconValue[p] = minusBox;
+                              }
+                            }
+                            if(!_Result[k].processesData[0].check_editable.processCleared){
+                              colorValue[p] = red 
+                              iconValue[p] = alert
+                              lock[p+1] = true;
+                            }
+                            
+                          }
+                          setRefresh(prevCount => prevCount + 1);
+                        }
+                        catch{
+
+                        }
+
+          
         }
+        console.log("COLOR VALUE___",colorValue);
+        console.log("INDEX---",netProgress);
+        setHeaderColor(colorValue[netProgress.length-1]);
+        setHeaderIcon(iconValue[netProgress.length-1]);
+        setHeaderText();
       }
-      catch{
-        return "minus-box"
+    },[resultsFetched]);
+
+    const setHeaderText = ()=>{
+      console.log("NET PROGRESS LENGTH--",netProgress.length,)
+
+      switch(colorValue[netProgress.length-1]){
+        case red:
+          console.log("RED")
+          if(netProgress.length<=preSurgeryProcessCount){
+            
+            setMessage("Not Cleared for start of day");
+          }else if(netProgress.length == processCount.length){
+            setMessage("Not Cleared for end of day");
+          }else{
+            var temp = "Not Cleared for surgery -0"+(netProgress.length-preSurgeryProcessCount)
+            setMessage(temp);
+          }
+          break;
+        case orange:
+          console.log("ORANGE")
+          if(netProgress.length<=preSurgeryProcessCount){
+            setMessage("Ongoing for start of day");
+          }else if(netProgress.length == processCount.length){
+            setMessage("Ongoing end of day");
+          }else{
+            var temp = "Ongoing for surgery -0"+(netProgress.length-preSurgeryProcessCount)
+            setMessage(temp);
+          }
+          break;
+        case green:
+          console.log("GREEN")
+          if(netProgress.length<preSurgeryProcessCount){
+            setHeaderColor(orange);
+            setHeaderIcon(minusBox);
+            setMessage("Ongoing for start of day");
+            break;
+          }
+          if(netProgress.length == preSurgeryProcessCount){
+            setMessage("Cleared for start of day");
+          }else if(netProgress.length == processCount.length){
+            setMessage("Cleared for end of day");
+          }else{
+            var temp = "Cleared for surgery -0"+(netProgress.length-preSurgeryProcessCount)
+            setMessage(temp);
+          }
+          break;
       }
-      return "minus-box"
+      if(netProgress[netProgress.length-1]==1){
+        if(colorValue[netProgress.length-1] == green){
+
+        }
+
+      }
+      
     }
 
     const changeColorSet=(id: number)=>{
@@ -193,10 +268,12 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
     const changeColorSetText=(id: number)=>{
       try{
         if(progress[id]){
-            if(moduleLock && id<preSurgeryProcessCount){
-              return "#f40000"
+          if(netProgress[id]==1){
+            if(colorValue[id] == green){
+              return colorValue[id];
             }
-            return colorValue[id];
+          }
+          
         }
       }
       catch{
@@ -208,19 +285,7 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
     const changeColorStyle=(processOrder: number,id: number,index: number)=>{
       try{
         if(progress[index]){
-            if(netProgress[index] == 1){
-              try{
-                if(netProgress[index+1]!= 1){
-                 _text = getText(processOrder,id,index);
-                }
-              }
-              catch{
-
-              }
-              if(moduleLock && index<preSurgeryProcessCount){
-                _text = "Not - "+_text;
-                return {color:"#f40000"};
-              }
+            if(colorValue[index] == green){
               return {color:colorValue[index]};
             }
             // else if(colorValue[id] == "#f40000")
@@ -246,7 +311,7 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
           </View>
           <View style={{width:30,height:30}}>
           <MaterialCommunityIcons
-          name={changeIconSet(index)} size={30} color={changeColorSet(index)}/>
+          name={iconValue[index]} size={30} color={changeColorSet(index)}/>
           </View>
           <View style={{width:2,height:40,backgroundColor:"#b6b6b6"}}>
           </View>
@@ -296,18 +361,18 @@ export default function preProcessScreen({route, navigation}: {navigation: any, 
         animated={true}
         backgroundColor="#006bcc"
         hidden={false} />
-        <View style={{width:"100%",height:80,
-        backgroundColor:"#ff8d48",justifyContent:"center",alignItems:"center",}}>  
+        {message != null ? (<View style={{width:"100%",height:80,
+        backgroundColor:headerColor,justifyContent:"center",alignItems:"center",}}>  
           <View style={{
             flexDirection:"row",}}>
           <MaterialCommunityIcons
-          name='minus-box' size={30} color='#ffffff'/>
+          name={headerIcon} size={30} color='#ffffff'/>
           <Text style={[styles.appButtonText,{flex:1, marginRight:14,color:"#ffffff",width:400,textAlignVertical:"center"}]}>
             {message}
           </Text>
           </View>
           
-        </View>
+        </View>):<></>}
         {renderFlatlistData && (
         <View style={{width:"100%",height:"100%",backgroundColor:"white"}}>
           <FlatList
